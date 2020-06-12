2e71b4b2a5d86c473633f0da08c6130f6c26a87d
automobileslie <carlie.anglemire@gmail.com>
2020-06-12 00:45:51 -0400

    edited submissionSuccess

--- client/src/SubmissionSuccess.js
+++ client/src/SubmissionSuccess.js
@@ -2,63 +2,35 @@ import React from 'react';
 
 export default class SubmissionSuccess extends React.Component{
 
-    //for a new direct request for item
-
-    forNewDirectRequestsForItems=()=>{
-        return <div>
-        <span className="submitted-new-listing-container">
-        <i role="img" className="submitted-new-listing-icon">&#10004;</i>
-        <h1>Your request has been submitted.</h1>
-        <p>When the donor approves the request, further directions will be provided for exchange.</p>
-        <button id="return-to-listings" onClick={this.props.returnToListingsIndex}>Return to Listings</button>
-        </span>
-    </div>
-    }
-
-    //for a new direct request to donate
-
-    forNewDirectRequestsToDonate=()=>{
-        return <div>
-            <span className="submitted-new-listing-container">
-            <i role="img" className="submitted-new-listing-icon">&#10004;</i>
-            <h1>Your donation has been submitted.</h1>
-            <p>When the requestor approves the submission, further directions will be provided for exchange.</p>
-            <button id="return-to-listings" onClick={this.props.returnToListingsIndex}>Return to Listings</button>
-            </span>
-        </div>
-    }
-
-    //for a new listing subnmitted
-
-    forNewListingSubmitted=()=>{
-
-    return <div>
-        <span className="submitted-new-listing-container">
-        <i role="img" className="submitted-new-listing-icon">&#10004;</i>
-        <h1>Your listing has been submitted.</h1>
-        <button id="return-to-listings" onClick={this.props.returnToListingsIndex}>Return to Listings</button>
-        </span>
-        </div>
-    }
 
     chooseWhichToRender=()=>{
         if(this.props.createdANewListing){
-           return this.forNewListingSubmitted()
+           return <h1>
+           Your listing has been submitted.</h1>
         }
+        
         else if(this.props.newDirectRequestForItem){
-            return this.forNewDirectRequestsForItems()
+            return <React.Fragment>
+            <h1>Your request has been submitted.</h1>
+            <p>When the donor approves the request, further directions will be provided for exchange.</p>
+            </React.Fragment>
         }
 
         else{
-            return this.forNewDirectRequestsToDonate()
+            return <React.Fragment>
+            <h1>Your donation has been submitted.</h1>
+            <p>When the requestor approves the submission, further directions will be provided for exchange.</p>
+            </React.Fragment>
         }
     }
 
 render(){
 return(
-
-this.chooseWhichToRender()
-
+        <span className="submitted-new-listing-container">
+        <i role="img" className="submitted-new-listing-icon">&#10004;</i>
+        {this.chooseWhichToRender()}
+        <button id="return-to-listings" onClick={this.props.returnToListingsIndex}>Return to Listings</button>
+        </span>
 )
 }
 }
